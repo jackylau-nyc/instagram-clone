4ce52b52f477b1841f19816091e222ba80c864a9
Jacky Lau <jackylau.email@gmail.com>
2021-01-29 03:05:57 -0500

    Minimum Viable Product

--- src/App.js
+++ src/App.js
@@ -1,40 +1,205 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Post from './Post.js';
+import { db, auth } from './firebase.js';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import { Button, Input } from '@material-ui/core';
+import ImageUpload from './ImageUpload.js';
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
 
 function App() {
 
-  const [posts, setPosts] = useState([
-    {
-      username: "justjacky",
-      caption: "King Tiger Panaque",
-      imageUrl: "https://cdn.shopify.com/s/files/1/1163/2672/products/L401-Royal-King-Alenquer-Pleco-12_1024x1024.jpg?v=1518710426"
-    }, 
-    {
-      username: "justjacky",
-      caption: "King Tiger Panaque",
-      imageUrl: "https://cdn.shopify.com/s/files/1/1163/2672/products/L401-Royal-King-Alenquer-Pleco-12_1024x1024.jpg?v=1518710426"
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle);
+
+  const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
+  const [openSignIn, setOpenSignIn] = useState('');
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if(authUser) {
+        //user has logged in
+        setUser(authUser);
+
+      }
+      else {
+        //user has logged out
+        setUser(null);
+      }
+    })
+
+    return () => {
+      //perform cleanup
+      unsubscribe();
     }
-  ]);
+  }, [user, username]);
+
+//Runs code whenever the variable content changes
+  useEffect(() => {
+    db.collection('posts').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
+      //everytime a post is added, this code runs
+      setPosts(snapshot.docs.map(doc => {
+        return ({
+        id: doc.id,
+        post: doc.data()
+      })}));
+    })
+  }, []);
+
+  const signUp = (event) => {
+    event.preventDefault();
+
+    auth
+    .createUserWithEmailAndPassword(email, password)
+    .then((authUser) => {
+      return authUser.user.updateProfile({
+        displayName: username
+      })
+    })
+    .catch((error) => alert(error.message));
+
+    setOpen(false);
+  }
+
+  const signIn = (event) => {
+    event.preventDefault();
+
+    auth.signInWithEmailAndPassword(email, password)
+    .catch((error) => alert(error.message));
+
+    setOpenSignIn(false);
+  }
 
   return (
 
-    <div className="App">
+    <div className="app">
+
+      <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <form className="app__signup">
+            <center>
+              <img
+                className="app__headerImage"
+                src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                alt=""
+              />
+            </center>
+            <Input
+              placeholder="username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Input
+              placeholder="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button type="submit" onClick={signUp}>Sign Up</Button>
+          </form>
+        </div>
+      </Modal>
+
+      <Modal
+        open={openSignIn}
+        onClose={() => setOpenSignIn(false)}
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <form className="app__signin">
+            <center>
+              <img
+                className="app__headerImage"
+                src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                alt=""
+              />
+            </center>
+            <Input
+              placeholder="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button type="submit" onClick={signIn}>Sign In</Button>
+          </form>
+        </div>
+      </Modal>
+
       <div className="app__header">
         <img
           className="app__headerImage"
           src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
           alt=""
         />
+        {user ? (
+        <Button onClick={() => auth.signOut()}>Logout</Button>
+        ): (
+          <div className="app__loginContainer">
+            <Button onClick={() => setOpenSignIn(true)}>Sign In</Button>
+            <Button onClick={() => setOpen(true)}>Sign Up</Button>
+          </div>
+        )}
       </div>
-      {
-        posts.map(post => {
-          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
-        })
-      }
-      {/* <Post username="justjacky" caption="King Tiger Panaque" imageUrl="https://cdn.shopify.com/s/files/1/1163/2672/products/L401-Royal-King-Alenquer-Pleco-12_1024x1024.jpg?v=1518710426" />
-      <Post username="dorea830" caption="Queen Arabesque" imageUrl="https://cdn.shopify.com/s/files/1/1163/2672/products/L260-Queen-Arabesque-Pleco-5_1024x1024.jpg?v=1510877535" />
-      <Post username="steliftw" caption="Leopard Frog" imageUrl="https://www.tropicalfishsite.com/wp-content/uploads/2013/04/leopard_frog_pleco.jpg" /> */}
+      
+      <div className="app__posts">
+        <div className="app__postsLeft">
+          {
+            posts.map(({id, post}) => 
+            //will rerender just the post with the edited id
+            <Post key={id} postId={id} user={user} username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
+            )
+          }
+        </div>
+      </div>
+
+      {user?.displayName ? (
+        <ImageUpload username={user.displayName} />
+      ): (
+        <h3>Sorry, please log in.</h3>
+      )}
     </div>
   );
 }
